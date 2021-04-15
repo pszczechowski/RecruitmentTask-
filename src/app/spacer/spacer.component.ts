fd7648cb4fffd3305cec3709196325ffc4e9be9d
Piotrek1991 <s16096@pjwstk.edu.pl>
2021-04-15 17:08:25 +0200

    remove spacer

--- src/app/spacer/spacer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-spacer',
-  template: `<div style="width: 100%; margin: {{ space }}px;"></div>`,
-})
-export class SpacerComponent {
-  @Input() space = '45'; // space between top and bottom of the spacer
-}
